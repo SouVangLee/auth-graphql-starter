d517081ee934bddc19a8384c6d3d783c84a84051
SouVangLee <sou_lee92@yahoo.com>
2022-08-16 17:01:30 -0700

    Update gitignore

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,10 +1,12 @@
 const graphql = require('graphql');
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLID } = graphql;
 const UserType = require('./user_type');
 
-// const RootQueryType = new GraphQLObjectType({
-//   name: 'RootQueryType'
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    dummyField: { type: graphql.GraphQLID }
+  }
+});
 
-// });
-
-// module.exports = RootQueryType;
+module.exports = RootQueryType;
