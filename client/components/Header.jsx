bd200b504ba1c26c16e5c3cbc1cfa6a8230e6327
SouVangLee <sou_lee92@yahoo.com>
2022-08-17 17:53:03 -0700

    Create Header component and add route

---
+++ client/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+  return (
+    <div>
+      I am the header
+    </div>
+  );
+};
+
+export default Header;
--- client/index.js
+++ client/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './components/App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache({
+    dataIdFromObject: obj => obj.id
+  }),
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client} >
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+        </Routes>
+      </HashRouter>
+    </ApolloProvider>
   );
 };
 
